5a8f570ad491090e459f3ac4acbcfa4381be77db
Umar <kulish662@gmail.com>
2024-01-20 10:22:52 +0300

    Initial commit

---
+++ src/main/java/com/pet/frchronic/FrchronicApplication.java
@@ -0,0 +1,13 @@
+package com.pet.frchronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrchronicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrchronicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet/frchronic/FrchronicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.frchronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrchronicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
